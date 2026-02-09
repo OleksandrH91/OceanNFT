80418850a5d5aa977aa93ff2be251a1200c0decf
OleksandrH91 <hurin.oleksandr91@gmail.com>
2026-02-09 08:00:39 +0200

    Добавил проект на ГИТ

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+  speed: 800, // Скорость самой анимации перелета слайда
+  
+  // Базовые настройки
+  slidesPerView: 1,      
+  spaceBetween: 20,     
+  loop: true,           
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  },
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
